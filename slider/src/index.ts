49d71f6666bffde919edf278ad66540ecc51fa19
Fespis <nikita.zhuk00@gmail.com>
2022-08-22 21:29:36 +0300

    feat: add swap function

--- slider/src/index.ts
+++ slider/src/index.ts
@@ -1,1 +1,32 @@
 import "./style.scss";
+
+const backgroundColors = [`#8dbdba`, `#8675ba`];
+
+function swapListItem() {
+  let currentItemNumber = 0;
+  const prev = document.querySelector(".prev");
+  const next = document.querySelector(".next");
+  const list = document.querySelector(".list") as HTMLElement;
+  const items = document.querySelectorAll(".list-item");
+
+  prev?.addEventListener("click", function () {
+    currentItemNumber -= 1;
+    if (currentItemNumber < 0) {
+      currentItemNumber = items.length - 1;
+    }
+    const currentItemWidth = items[currentItemNumber]?.scrollWidth;
+    list.style.marginRight = `${currentItemWidth * currentItemNumber}px`;
+    list.style.backgroundColor = `${backgroundColors[currentItemNumber]}`;
+  });
+
+  next?.addEventListener("click", function () {
+    currentItemNumber += 1;
+    if (currentItemNumber > items.length - 1) {
+      currentItemNumber = 0;
+    }
+    const currentItemWidth = items[currentItemNumber]?.scrollWidth;
+    list.style.marginRight = `${currentItemWidth * currentItemNumber}px`;
+    list.style.backgroundColor = `${backgroundColors[currentItemNumber]}`;
+  });
+}
+swapListItem();
