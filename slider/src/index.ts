4634fc3a3d968020670582728d3fbdc5877b4210
Fespis <nikita.zhuk00@gmail.com>
2022-08-23 18:30:23 +0300

    feat: add swap function for all buttons

--- slider/src/index.ts
+++ slider/src/index.ts
@@ -1,38 +1,41 @@
 import "./style.scss";
 
-const backgroundColors = [`#8dbdba`, `#8675ba`];
-
 function swapListItem() {
   let currentItemNumber = 0;
-  const prev = document.querySelector(".menu_buttons__prev");
-  const next = document.querySelector(".menu_buttons__next");
+  const backgroundColors = [`#8dbdba`, `#8675ba`];
+  const prevButtons = document.querySelectorAll(".menu_buttons__prev");
+  const nextButtons = document.querySelectorAll(".menu_buttons__next");
   const list = document.querySelector(".list") as HTMLElement;
   const items = document.querySelectorAll(".list_item");
 
-  prev?.addEventListener("click", function () {
-    currentItemNumber -= 1;
-    if (currentItemNumber < 0) {
-      currentItemNumber = items.length - 1;
-    }
-    const currentItem = items[currentItemNumber] as HTMLElement;
-    const currentItemWidth = currentItem?.scrollWidth;
-    (items[0] as HTMLElement).style.marginLeft = `-${
-      currentItemWidth * currentItemNumber
-    }px`;
-    list.style.backgroundColor = `${backgroundColors[currentItemNumber]}`;
+  prevButtons.forEach((prevButton) => {
+    prevButton?.addEventListener("click", function () {
+      currentItemNumber -= 1;
+      if (currentItemNumber < 0) {
+        currentItemNumber = items.length - 1;
+      }
+      const currentItem = items[currentItemNumber] as HTMLElement;
+      const currentItemWidth = currentItem?.scrollWidth;
+      (items[0] as HTMLElement).style.marginLeft = `-${
+        currentItemWidth * currentItemNumber
+      }px`;
+      list.style.backgroundColor = `${backgroundColors[currentItemNumber]}`;
+    });
   });
 
-  next?.addEventListener("click", function () {
-    currentItemNumber += 1;
-    if (currentItemNumber > items.length - 1) {
-      currentItemNumber = 0;
-    }
-    const currentItem = items[currentItemNumber] as HTMLElement;
-    const currentItemWidth = currentItem?.scrollWidth;
-    (items[0] as HTMLElement).style.marginLeft = `-${
-      currentItemWidth * currentItemNumber
-    }px`;
-    list.style.backgroundColor = `${backgroundColors[currentItemNumber]}`;
+  nextButtons.forEach((nextButton) => {
+    nextButton?.addEventListener("click", function () {
+      currentItemNumber += 1;
+      if (currentItemNumber > items.length - 1) {
+        currentItemNumber = 0;
+      }
+      const currentItem = items[currentItemNumber] as HTMLElement;
+      const currentItemWidth = currentItem?.scrollWidth;
+      (items[0] as HTMLElement).style.marginLeft = `-${
+        currentItemWidth * currentItemNumber
+      }px`;
+      list.style.backgroundColor = `${backgroundColors[currentItemNumber]}`;
+    });
   });
 }
 swapListItem();
