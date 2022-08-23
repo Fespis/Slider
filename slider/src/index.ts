b6b1e2731656e09218edb39f5ad7d657567f3ebe
Fespis <nikita.zhuk00@gmail.com>
2022-08-23 20:15:47 +0300

    feat: add new cursor

--- slider/src/index.ts
+++ slider/src/index.ts
@@ -1,5 +1,31 @@
 import "./style.scss";
 
+function changeCursor() {
+  const newCursor = document.querySelector(".cursor") as HTMLElement;
+  newCursor.style.width = "100px";
+  newCursor.style.height = "100px";
+}
+
+function reChangeCursor() {
+  const newCursor = document.querySelector(".cursor") as HTMLElement;
+  newCursor.style.width = "50px";
+  newCursor.style.height = "50px";
+}
+
+function moveNewCursor() {
+  const newCursor = document.querySelector(".cursor") as HTMLElement;
+  window.addEventListener("mousemove", (e) => {
+    const mouseY = e.clientY;
+    const mouseX = e.clientX;
+    const centerX = newCursor.offsetLeft + newCursor.offsetWidth / 2;
+    const centerY = newCursor.offsetTop + newCursor.offsetHeight / 2;
+    const OFFSET_FROM_CENTER = 10;
+    newCursor.style.transform = `translate3d(${
+      mouseX - centerX + OFFSET_FROM_CENTER
+    }px, ${mouseY - centerY + OFFSET_FROM_CENTER}px, 0)`;
+  });
+}
+
 function swapListItem() {
   let currentItemNumber = 0;
   const backgroundColors = [`#8dbdba`, `#8675ba`];
@@ -9,7 +35,13 @@ function swapListItem() {
   const items = document.querySelectorAll(".list_item");
 
   prevButtons.forEach((prevButton) => {
-    prevButton?.addEventListener("click", function () {
+    prevButton?.addEventListener("mouseover", () => {
+      changeCursor();
+    });
+    prevButton?.addEventListener("mouseout", () => {
+      reChangeCursor();
+    });
+    prevButton?.addEventListener("click", () => {
       currentItemNumber -= 1;
       if (currentItemNumber < 0) {
         currentItemNumber = items.length - 1;
@@ -24,7 +56,13 @@ function swapListItem() {
   });
 
   nextButtons.forEach((nextButton) => {
-    nextButton?.addEventListener("click", function () {
+    nextButton?.addEventListener("mouseover", () => {
+      changeCursor();
+    });
+    nextButton?.addEventListener("mouseout", () => {
+      reChangeCursor();
+    });
+    nextButton?.addEventListener("click", () => {
       currentItemNumber += 1;
       if (currentItemNumber > items.length - 1) {
         currentItemNumber = 0;
@@ -38,4 +76,6 @@ function swapListItem() {
     });
   });
 }
+
+moveNewCursor();
 swapListItem();
